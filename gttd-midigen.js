84f3d5835b4b36b3b5067d3b2d70f025a8789fd1
Tom Wadley <tom@tomwadley.net>
2016-02-01 17:16:17 +0000

    Clean up code a bit

--- gttd-midigen.js
+++ gttd-midigen.js
@@ -13,18 +13,11 @@ var ROOT_SUBMEDIANT = 5;
 var ROOT_LEADING = 6;
 
 function chord(track, notes, duration) {
-  track.noteOn(0, notes[0], 0);
-  for (var i = 1; i < notes.length; i++) {
-    track.noteOn(0, notes[i]);
-  }
-  track.noteOff(0, notes[0], duration);
-  for (var i = 1; i < notes.length; i++) {
-    track.noteOff(0, notes[i]);
-  }
+  chordWithVolume(track, notes, duration, 90);
 }
 
-function quietChord(track, notes, duration) {
-  track.noteOn(0, notes[0], 0, 30);
+function chordWithVolume(track, notes, duration, volume) {
+  track.noteOn(0, notes[0], 0, volume);
   for (var i = 1; i < notes.length; i++) {
     track.noteOn(0, notes[i]);
   }
@@ -86,8 +79,14 @@ function chordNotes(root, scale) {
 var prisoner = {
   options: ['cooperate', 'defect'],
   callbacks: {
-    'cooperate': [player1Cooperate, player2Cooperate],
-    'defect': [player1Defect, player2Defect]
+    'cooperate': [
+      function(track, nash) { prisonersDilemaProgression(track, minorScale(NOTE_C), nash); },
+      function(track, nash) { prisonersDilemaProgression(track, minorScale(NOTE_A), nash); }
+    ],
+    'defect': [
+      function(track, nash) { prisonersDilemaProgression(track, majorScale(NOTE_C), nash); },
+      function(track, nash) { prisonersDilemaProgression(track, majorScale(NOTE_A), nash); }
+    ]
   },
   payoffMatrix: {
     'cooperate': { 'cooperate': [-1, -1], 'defect': [-10, 0] },
@@ -97,8 +96,14 @@ var prisoner = {
 var chicken = {
   options: ['flight', 'fight'],
   callbacks: {
-    'flight': [player1Flight, player2Flight],
-    'fight': [player1Fight, player2Fight]
+    'flight': [
+      function(track, nash) { fightFlightProgression(track, nash, ROOT_SUBMEDIANT, ROOT_SUBDOMINANT, true); },
+      function(track, nash) { fightFlightProgression(track, nash, ROOT_SUPERTONIC, ROOT_TONIC, false); }
+    ],
+    'fight': [
+      function(track, nash) { fightFlightProgression(track, nash, ROOT_SUBDOMINANT, ROOT_DOMINANT, true); },
+      function(track, nash) { fightFlightProgression(track, nash, ROOT_MEDIANT, ROOT_SUPERTONIC, false); }
+    ]
   },
   payoffMatrix: {
     'flight': { 'flight': [0, 0], 'fight': [-1, 1] },
@@ -108,9 +113,18 @@ var chicken = {
 var rockPaperScissors = {
   options: ['paper', 'scissors', 'rock'],
   callbacks: {
-    'paper': [player1Paper, player2Paper],
-    'scissors': [player1Scissors, player2Scissors],
-    'rock': [player1Rock, player2Rock]
+    'paper': [
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_SUBDOMINANT, NOTE_C, first, last, wins); },
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_SUBDOMINANT, NOTE_A, first, last, wins); }
+    ],
+    'scissors': [
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_DOMINANT, NOTE_C, first, last, wins); },
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_DOMINANT, NOTE_A, first, last, wins); }
+    ],
+    'rock': [
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_TONIC, NOTE_C, first, last, wins); },
+      function(track, nash, first, last, wins) { rpsProgression(track, ROOT_TONIC, NOTE_A, first, last, wins); }
+    ]
   },
   payoffMatrix: {
     'paper': { 'paper': [0, 0], 'scissors': [-1, 1], 'rock': [1, -1] },
@@ -142,68 +156,60 @@ function playGame(track, game) {
   var nash = [];
   
   for (var i = 0; i < game.options.length; i++) {
-    var maxPlayer1 = NaN;
-    var maxPlayer1Option = null;
+    var possiblePlayer1Nash = null;
+    var possiblePlayer2Nash = null;
+
     var maxPlayer2 = NaN;
-    var maxPlayer2Option = null;
 
     for (var j = 0; j < game.options.length; j++) {
-      player1 = game.options[i];
-      player2 = game.options[j];
-      payoff = game.payoffMatrix[player1][player2];
+      var player1 = game.options[i];
+      var player2 = game.options[j];
+      var payoff = game.payoffMatrix[player1][player2];
       console.log("Player 1: " + player1 + ", Player 2: " + player2 + ", Payoff: " + payoff);
 
       if (game.hasOwnProperty('callbacks')) {
-        player1Callback = game.callbacks[player1][0];
-        player2Callback = game.callbacks[player2][1];
-        var first = false;
-        var last = false;
-        if (i == 0 && j == 0) {
-          first = true;
-        }
-        if (i == game.options.length - 1 && j == game.options.length - 1) {
-          last = true;
-        }
+        var player1Callback = game.callbacks[player1][0];
+        var player2Callback = game.callbacks[player2][1];
+        var first = i == 0 && j == 0;
+        var last = i == game.options.length - 1 && j == game.options.length - 1;
         var player1Wins = payoff[0] >= payoff[1];
         var player2Wins = payoff[1] >= payoff[0];
         player1Callback(track, false, first, false, player1Wins);
         player2Callback(track, false, false, last, player2Wins);
       }
 
-      var possibleNash = true;
+      var possibleNash = false;
 
       if (isNaN(maxPlayer2) || payoff[1] > maxPlayer2) {
         maxPlayer2 = payoff[1];
-        maxPlayer2Option = player2;
+        possibleNash = true;
+        possiblePlayer1Nash = null;
+        possiblePlayer2Nash = null;
 
         for (var k = 0; k < game.options.length; k++) {
           if (k == i) continue;
-          payoffPrime = game.payoffMatrix[game.options[k]][game.options[j]];
+          var payoffPrime = game.payoffMatrix[game.options[k]][game.options[j]];
           if (payoffPrime[0] > payoff[0]) {
             possibleNash = false;
-            maxPlayer1 = NaN;
-            maxPlayer1Option = null;
             break;
           }
         }
-      } else {
-        possibleNash = false;
       }
 
       if (possibleNash) {
-        maxPlayer1 = payoff[0];
-        maxPlayer1Option = player1;
+        possiblePlayer1Nash = player1;
+        possiblePlayer2Nash = player2;
       }
     }
 
-    if (maxPlayer1Option != null && maxPlayer2Option != null) {
-      nash.push([maxPlayer1Option, maxPlayer2Option]);
-      console.log("Nash equilibrium found! Player 1: " + maxPlayer1Option + ", Player 2: " + maxPlayer2Option);
+    if (possiblePlayer1Nash != null && possiblePlayer2Nash != null) {
+      nash.push([possiblePlayer1Nash, possiblePlayer2Nash]);
+      console.log("Nash equilibrium found! Player 1: " + possiblePlayer1Nash + ", Player 2: " + possiblePlayer2Nash);
     }
   }
 
   for (var i = 0; i < nash.length; i++) {
-    ne = nash[i];
+    var ne = nash[i];
     
     if (game.hasOwnProperty('callbacks')) {
       player1Callback = game.callbacks[ne[0]][0];
@@ -229,68 +235,27 @@ function prisonersDilemaProgression(track, scale, nash) {
   }
 }
 
-function player1Defect(track, nash) {
-  prisonersDilemaProgression(track, majorScale(NOTE_C), nash);
-}
-
-function player1Cooperate(track, nash) {
-  prisonersDilemaProgression(track, minorScale(NOTE_C), nash);
-}
-
-function player2Defect(track, nash) {
-  prisonersDilemaProgression(track, majorScale(NOTE_A), nash);
-}
-
-function player2Cooperate(track, nash) {
-  prisonersDilemaProgression(track, minorScale(NOTE_A), nash);
-}
-
 function rpsProgression(track, root, scale_note, first, last, wins) {
-  var scale;
-  if (wins) {
-    scale = majorScale(scale_note);
-  } else {
-    scale = minorScale(scale_note);
-  }
+  var scale = wins ? majorScale(scale_note) : minorScale(scale_note);
+
   if (last) {
     track.note(0, scale(root - 1), 64, 48);
     chord(track, chordNotes(root, scale), 192);
   } else if (first) {
     track.note(0, scale(root - 1), 64);
     chord(track, chordNotes(root, scale), 96);
-    quietChord(track, chordNotes(root, scale), 48);
+    chordWithVolume(track, chordNotes(root, scale), 48, 30);
   } else {
     track.note(0, scale(root - 1), 64, 48);
     chord(track, chordNotes(root, scale), 96);
-    quietChord(track, chordNotes(root, scale), 48);
+    chordWithVolume(track, chordNotes(root, scale), 48, 30);
   }
-}
-
-function player1Rock(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_TONIC, NOTE_C, first, last, wins);
-}
-function player1Paper(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_SUBDOMINANT, NOTE_C, first, last, wins);
-}
-function player1Scissors(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_DOMINANT, NOTE_C, first, last, wins);
-}
-function player2Rock(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_TONIC, NOTE_A, first, last, wins);
-}
-function player2Paper(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_SUBDOMINANT,NOTE_A, first, last, wins);
-}
-function player2Scissors(track, nash, first, last, wins) {
-  rpsProgression(track, ROOT_DOMINANT, NOTE_A, first, last, wins);
 }
 
 function fightFlightProgression(track, nash, first_root, second_root, split) {
   var scale = majorScale(NOTE_C);
-  var func = chord;
-  if (nash) {
-    func = spread;
-  }
+  var func = nash ? spread : chord;
+
   func(track, chordNotes(first_root, scale), 256);
   if (split) {
     func(track, chordNotes(second_root, scale), 128);
@@ -298,17 +263,4 @@ function fightFlightProgression(track, nash, first_root, second_root, split) {
   } else {
     func(track, chordNotes(second_root, scale), 256);
   }
-}
-
-function player1Fight(track, nash) {
-  fightFlightProgression(track, nash, ROOT_SUBDOMINANT, ROOT_DOMINANT, true);
-}
-function player1Flight(track, nash) {
-  fightFlightProgression(track, nash, ROOT_SUBMEDIANT, ROOT_SUBDOMINANT, true);
-}
-function player2Fight(track, nash) {
-  fightFlightProgression(track, nash, ROOT_MEDIANT, ROOT_SUPERTONIC, false);
-}
-function player2Flight(track, nash) {
-  fightFlightProgression(track, nash, ROOT_SUPERTONIC, ROOT_TONIC, false);
 }
